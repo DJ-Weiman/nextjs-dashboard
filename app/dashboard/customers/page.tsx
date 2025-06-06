436f91eee9de759ac90bfbe4d2713b09bf3d5556
Dion Weiman <dion@4axissolutions.com>
2025-06-06 16:21:43 +0530

    Setup Nav

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <p>Customers page</p>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <p>Invoices page</p>
+  )
+}
+
+export default page
